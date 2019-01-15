e2140d4bc3a52b492f332df7280ca7114cb861d7
Moshe Ben-Shoham <mosheb@perfectomobile.com>
2019-01-15 16:48:53 +0200

    working on jenkinsFile

--- nodejs/repository-manager/__tests__/getUploadLink.test.js
+++ nodejs/repository-manager/__tests__/getUploadLink.test.js
@@ -13,7 +13,7 @@ var getUploadLinkResponse;
 test('create upload link', () => {
     // get upload link
     getUploadLinkResponse = getUploadLinkHandler.getUploadLink(bucketName,objectPath);
-    expect(getUploadLinkResponse.statusCode).toBe(300);
+    expect(getUploadLinkResponse.statusCode).toBe(200);
 });
     
 test('use upload link', done => {
